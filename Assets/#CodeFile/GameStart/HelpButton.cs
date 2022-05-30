07dc55ee6fce9de17c8ce6aa21697af55941b327
ParkWooJae <mhpark2696@naver.com>
2022-05-30 19:36:25 +0900

    씬 추가 및 화면전환
    도움말, 환경설정 화면 가
    버튼에 각각 화면전환 코드 추가

---
+++ Assets/#CodeFile/GameStart/HelpButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class HelpButton : MonoBehaviour
+{
+    Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClickButton);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnClickButton(){
+        SceneManager.LoadScene("Help");
+    }
+}
